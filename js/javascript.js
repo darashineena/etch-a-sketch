771615c740d698c72dd940ad75e5e2eb80b5b39d
darashi <19747068+darashineena@users.noreply.github.com>
2023-03-11 18:02:27 +0100

    Create an input box controlling row amount

--- js/javascript.js
+++ js/javascript.js
@@ -1,17 +1,33 @@
-const container = document.querySelector(".container")
-const maxShrinkWidth = container.offsetWidth;
-let rowsAmount = 16;
+const container = document.querySelector(".container");
 
-for (let i = 0; i < rowsAmount ** 2; i++) {
-    const cell = document.createElement('div');
-    cell.classList.add('cell');
-    cell.style.cssText = `flex-basis: 40px`;
-    cell.addEventListener('mouseover', () => {  //also on click?
-        colorCell(cell)
-    });
-    container.appendChild(cell);
+const rowsAmountInput = document.querySelector("#row-number");
+rowsAmountInput.defaultValue = 16;
+let rowsAmount = rowsAmountInput.value;
+rowsAmountInput.addEventListener('change', resetCanvas) 
+const button = document.querySelector("#reset")
+button.addEventListener('click', resetCanvas)
+
+createCanvas(rowsAmount);
+
+function createCanvas(rowsAmount) {
+    for (let i = 0; i < rowsAmount ** 2; i++) {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        cell.style["flex-basis"] = `${640 / rowsAmount}px`;
+        cell.addEventListener('mouseover', () => {  //also on click?
+            colorCell(cell)
+        });
+        container.appendChild(cell);
+    }
+}
+
+function resetCanvas() {
+    rowsAmount = parseInt(rowsAmountInput.value);
+    if (rowsAmount > 100) rowsAmount = 100;
+    document.querySelectorAll('.cell').forEach(e => e.remove())
+    createCanvas(rowsAmount)
 }
 
 function colorCell(selectedCell) {
-    selectedCell.style.cssText = "flex-basis: 40px; background-color: blue";
+    selectedCell.style["background-color"] = "blue";
 }
